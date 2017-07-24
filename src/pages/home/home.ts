3bda0e7f51c0e49f73b7a91ad99570420a245e94
JonathanNau <jonathan_nau@live.com>
2017-07-24 19:26:44 -0300

    Commit inicial

---
+++ src/pages/home/home.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { MenuController } from 'ionic-angular';
+import { Geo } from '../geo/geo';
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  geo = Geo;
+  constructor(public navCtrl: NavController, public menuCtrl: MenuController, public navParams: NavParams) {
+    
+  }
+
+  pushPage(){
+    // push another page onto the navigation stack
+    // causing the nav controller to transition to the new page
+    // optional data can also be passed to the pushed page.
+    this.navCtrl.push(Geo, {});
+  }
+}
