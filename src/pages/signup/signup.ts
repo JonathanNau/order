177be20159508ba02ea462fd63fe8e69353b70d5
JonathanNau <jonathan_nau@live.com>
2017-08-10 14:06:15 -0300

    Criação de página para criar usuário

---
+++ src/pages/signup/signup.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { LoginProvider } from '../../providers/loginprovider'
+/**
+ * Generated class for the Signup page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-signup',
+  templateUrl: 'signup.html',
+})
+export class Signup {
+  usercreds = {
+            name: '',
+            password: ''
+  };
+  constructor(public navCtrl: NavController, public navParams: NavParams, public loginprovider: LoginProvider, public alertCtrl: AlertController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad Signup');
+  }
+
+  register(user) {
+    this.loginprovider.adduser(user).then(data => {
+            if(data) {
+                var alert = this.alertCtrl.create({
+                    title: 'Success',
+                    subTitle: 'User Created',
+                    buttons: ['ok']
+                });
+                alert.present();
+            }
+    });
+    console.log(user)
+  }
+
+}
