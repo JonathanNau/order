595a2eea84a0f51f5cc41dd077a05581327106e8
JonathanNau <jonathan_nau@live.com>
2017-09-08 22:28:02 -0300

    Criado novos métodos para obter e modificar as categorias

--- src/providers/json.ts
+++ src/providers/json.ts
@@ -7,10 +7,32 @@ import { LoginProvider } from '../providers/loginprovider'
 @Injectable()
 export class Json {
   data: any;
+  base_url = 'http://localhost:8000/api';
   constructor(public http: Http, public lp: LoginProvider) {
     console.log('Hello rapaze');
   }
+  
+  getCategoriaData(){
+      var headers = new Headers();
+      headers.append('Authorization','JWT ' +this.lp.getCredentials());
+      return this.http.get(this.base_url+'/categoria/?format=json', {headers: headers})
+      .map(res => res.json())
+  }
 
+  alterarCategoria(data){
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json' );
+    headers.append('Authorization','JWT ' +this.lp.getCredentials());
+
+    return this.http.put(this.base_url+'/categoria/'+data.id+'/', data, {headers: headers})
+    .map(res => res.json())
+    .subscribe(data => {
+      console.log(data['_body']);
+      }, error => {
+      console.log(error);
+    });
+  }
+/*
   getData(){
     return this.http.get('http://localhost:8000/api/usuarios/?format=json').map(res => res.json());
   }
@@ -18,18 +40,6 @@ export class Json {
   getUsuariosData(){
     return this.http.get('http://localhost:8000/api/usuarios/?format=json').map(res => res.json());
   }
-
-  getCategoriaData(){
-
-      var headers = new Headers();
-      headers.append('Authorization','JWT ' +this.lp.getCredentials());
-      console.log(this.lp.getCredentials());
-      console.log(headers);
-      return this.http.get('http://localhost:8000/api/categoria/?format=json', {headers: headers})
-      .map(res => res.json())
-
-  }
-
   postRequest() {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json' );
@@ -52,5 +62,5 @@ export class Json {
         console.log(error);
       });
   }
-
+*/
 }
